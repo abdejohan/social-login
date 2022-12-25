c9112641b0c7fea6894f2f4f576cdc28a4541d30
abdejohan <abdejohan@gmail.com>
2022-12-26 00:09:54 +0100

    Add .env

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,20 +1,12 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { firebaseConfig } from "../env";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-	apiKey: "AIzaSyAhsjdiP2mxoDRXXpQuz8Pco64zrA7oqjk",
-	authDomain: "social-login-8a192.firebaseapp.com",
-	projectId: "social-login-8a192",
-	storageBucket: "social-login-8a192.appspot.com",
-	messagingSenderId: "611002235396",
-	appId: "1:611002235396:web:9afb9a021e3e85a4d81351",
-	measurementId: "G-WQZ6HWJQCT",
-};
 
 // Initialize Firebase
 const initializeFirebaseApp = () => {
