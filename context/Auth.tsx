f05c9907c66cd842c8c899668ccdfce5f64724d7
abdejohan <abdejohan@gmail.com>
2023-01-16 13:38:16 +0100

    Remove old style of storing access token

--- context/Auth.tsx
+++ context/Auth.tsx
@@ -5,16 +5,13 @@ import React, {
 	useCallback,
 	useEffect,
 } from "react";
-import useAsyncStorage from "../hooks/useAsyncStorage";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 
 type ContextType = {
-	token: string | null;
 	isSignedIn: boolean;
 	user: object;
 	setUser: (value: object) => void;
 	setIsSignedIn: (value: boolean) => void;
-	setToken: (value: string) => void;
 	handleSignIn: (userCredential: any) => void;
 	handleSignOut: () => void;
 };
@@ -24,12 +21,10 @@ type AuthContextProps = {
 };
 
 const AuthContext = React.createContext<ContextType>({
-	token: null,
 	isSignedIn: false,
 	user: {},
 	setIsSignedIn: () => {},
 	setUser: () => {},
-	setToken: () => {},
 	handleSignIn: () => {},
 	handleSignOut: () => {},
 });
@@ -37,86 +32,62 @@ const AuthContext = React.createContext<ContextType>({
 export const AuthContextProvider: FunctionComponent<AuthContextProps> = (
 	props: AuthContextProps
 ) => {
-	const [token, setToken] = useState<string | null>(null);
 	const [user, setUser] = useState<{}>({});
 	const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
-	const { getData, storeData, removeData } = useAsyncStorage();
 
-	const fetchToken = useCallback(async () => {
+	const checkAuthState = useCallback(async () => {
 		const auth = getAuth();
+
 		onAuthStateChanged(auth, (user) => {
 			if (user) {
-				handleSignIn(user);
-				console.log(user);
 				// User is signed in, see docs for a list of available properties
 				// https://firebase.google.com/docs/reference/js/firebase.User
+				handleSignIn(user);
 				const uid = user.uid;
-				// ...
 			} else {
-				console.log("COULD NOT FIND A USER!");
+				console.log("Did not find a valid token, no user is signed in.");
 				// User is signed out
 				// ...
 			}
 		});
-		//const accessToken = await getData("ACCESS_TOKEN");
-	}, []);
-
-	const saveTokenToStorage = useCallback(async (t: string) => {
-		if (typeof t === "string") {
-			//await storeData("ACCESS_TOKEN", t);
-			setToken(t);
-			setIsSignedIn(true);
-		}
 	}, []);
 
 	const handleSignIn = useCallback(async (user: any) => {
-		const { accessToken, email } = user;
+		const { email } = user;
 		setUser({ email });
-		saveTokenToStorage(accessToken);
+		setIsSignedIn(true);
 		console.log(`
 			User signed in:
-			accessToken: ${accessToken}
 			email: ${email}
 			`);
 	}, []);
 
 	const handleSignOut = useCallback(async () => {
-		const auth = getAuth();
-		await signOut(auth)
-			.then(() => console.log("Sign-out successful."))
-			.catch((error) => console.log("An error happened: " + error));
-		// await removeData("ACCESS_TOKEN");
-		setToken(null);
-		setIsSignedIn(false);
+		try {
+			const auth = getAuth();
+			await signOut(auth);
+			setIsSignedIn(false);
+			console.log("Sign out successful.");
+		} catch (error) {
+			console.log("An error happened, could not sign out user: " + error);
+		}
 	}, []);
 
 	useEffect(() => {
-		// Only runs once
-		// Fetches access token
-		fetchToken();
+		// Only runs once, and checks for a access token
+		checkAuthState();
 	}, []);
 
 	const state = useMemo(
 		() => ({
-			token,
 			isSignedIn,
 			user,
 			setUser,
 			setIsSignedIn,
-			setToken,
 			handleSignIn,
 			handleSignOut,
 		}),
-		[
-			token,
-			isSignedIn,
-			user,
-			setUser,
-			setIsSignedIn,
-			setToken,
-			handleSignIn,
-			handleSignOut,
-		]
+		[isSignedIn, user, setUser, setIsSignedIn, handleSignIn, handleSignOut]
 	);
 
 	return <AuthContext.Provider value={state}>{props.children}</AuthContext.Provider>;
