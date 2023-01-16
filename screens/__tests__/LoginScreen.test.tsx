333ee025d0f77d5a6cc95ecd072f085c88d35bdf
abdejohan <abdejohan@gmail.com>
2023-01-16 12:40:28 +0100

    Work on testing

---
+++ screens/__tests__/LoginScreen.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen, cleanup, fireEvent } from "@testing-library/react-native";
+import LoginScreen from "../LoginScreen";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+	signInWithRedirect,
+	GoogleAuthProvider,
+	FacebookAuthProvider,
+	signInWithPopup,
+} from "firebase/auth";
+jest.useFakeTimers();
+afterEach(cleanup);
+
+/**
+ * jest.mock("firebase/auth", () => {
+	return {
+		getAuth: jest.fn(),
+		signInWithEmailAndPassword: jest.fn(),
+	};
+});
+ */
+
+describe("Test Login screen", () => {
+	test("Should go to the Home page on login", async () => {
+		const mockNavigation = { navigate: () => {} };
+		render(<LoginScreen navigation={mockNavigation} />);
+		//screen.debug({ message: "HERE WE ARE" });
+		// const signInWithEmailAndPassword = jest.fn();
+		// const getAuth = jest.fn();
+		const signUpButton = screen.getByText("Sign up");
+		//const signInButton = screen.getByTestId("sign-in-button");
+		expect(signUpButton).toBeTruthy();
+		// fireEvent.press(signInButton);
+		// (getAuth).toHaveBeenCalled();
+		// expect(signInWithEmailAndPassword).toHaveBeenCalled();
+	});
+});
