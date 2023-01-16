948d91bf8529120387e7e662fe5e138f1e795c73
abdejohan <abdejohan@gmail.com>
2023-01-16 17:45:31 +0100

    Add example.env.ts

---
+++ example.env.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAhsjdiP2mxoDRXXpQuz8Pco64zrA7oqjk",
+	authDomain: "social-login-8a192.firebaseapp.com",
+	projectId: "social-login-8a192",
+	storageBucket: "social-login-8a192.appspot.com",
+	messagingSenderId: "611002235396",
+	appId: "1:611002235396:web:9afb9a021e3e85a4d81351",
+	measurementId: "G-WQZ6HWJQCT",
+};
+
+export { firebaseConfig };
